63181020ef10feb9cdcfdacbd5e11f34b4cad196
Ralph Lipe <ralph@lipe.ws>
2024-09-28 21:36:53 -0700

    Improved test

--- Tests/ContractBridgeTests/EventTest.swift
+++ Tests/ContractBridgeTests/EventTest.swift
@@ -14,11 +14,17 @@ class EventTests: XCTestCase {
         let event = Event()
         
         event.boards.append(Board(number: 1, deal: try Deal(from: "N:AKQJ.AKQ.AKQ.AKQ T98.JT98.JT9.JT9 765.765.8765.876 432.432.432.5432")))
+        event.boards.append(Board(number: 2, deal: try Deal(from: "N:AKQJ.AKQ.AKQ.AKQ T98.JT98.JT9.JT9 765.765.8765.876 432.432.432.5432")))
         
-        event.pairs.append(Pair(number: 1, direction: .ns, player0: "Ralph Lipe", player1: "Lynda Lipe"))
+        event.pairs.append(Pair(number: 1, direction: .ns, player0: "Ralph", player1: "Lynda"))
+        event.pairs.append(Pair(number: 2, direction: .ns, player0: "James", player1: "Claude"))
+        event.pairs.append(Pair(number: 1, direction: .ew, player0: "Nicole", player1: "Max"))
+        event.pairs.append(Pair(number: 2, direction: .ew, player0: "Gary", player1: "Karina"))
         
+        event.boardResults.append(BoardResult(boardNumber: 1, nsPairNumber: 1, ewPairNumber: 2, contract: Contract(level: 3, strain: .noTrump, risk: .undoubled), declarer: .north, score: 400))
      //   event.boardResults.Append(BoardResult()
         let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
         do {
             let jsonData = try encoder.encode(event)
             if let jsonString = String(data: jsonData, encoding: .utf8) {
